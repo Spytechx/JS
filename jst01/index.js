1fe7cc3653679b3c6161251fb961294586f6d33d
Spytechx <samuraidovexx@gmail.com>
2022-06-06 15:03:42 +0100

    JS

---
+++ jst01/index.js
@@ -0,0 +1,20 @@
+let africancountries = [
+    "Nigeria",
+    "Ethopia",
+    "Ghana",
+    "Benin Republic",
+    "Cameroon",
+    "Togo",
+    "Gabon",
+    "Mali",
+    "Angola",
+    "Sudan",
+    "Tanzania",
+]
+
+for(let x = 0; x<africancountries.length; x++)
+// {
+//     console.log("I am " + africancountries[x] + " a country in Africa");
+// }
+
+alert("I am " + africancountries[x] + " a country in Africa");
